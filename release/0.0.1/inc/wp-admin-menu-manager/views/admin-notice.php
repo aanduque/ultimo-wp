640fa8fc075c810d4bd44e2cb4243f25633d0403
Arindo Duque <aanduque@gmail.com>
2015-04-05 16:18:51 -0300

    .gitignore is now working

--- release/0.0.1/inc/wp-admin-menu-manager/views/admin-notice.php
+++
@@ -1,3 +0,0 @@
-<div class="updated">
-    <p><?php _e('Keep calm! This menu is only available for high-level users (Administrators). Users with lower capabilities won\'t be able to see or edit a thing here!.', $this->text_domain); ?></p>
-</div>
